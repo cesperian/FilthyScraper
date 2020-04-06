fc04cc2a49ed1a9ef6179f94dd8d0c184e5babfa
cesperian <cesperian@gmail.com>
2020-04-06 15:44:13 +0700

    set up basic navigation

--- filthyarchive/filthyarchive/spiders/archive.py
+++ filthyarchive/filthyarchive/spiders/archive.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-
 class ArchiveSpider(scrapy.Spider):
-    name = 'archive'
-    allowed_domains = ['http://www.millbankusa.com/filthycritic/recent-shit']
-    start_urls = ['http://http://www.millbankusa.com/filthycritic/recent-shit/']
+    name = 'getarchive'
+    # allowed_domains = ['http://www.millbankusa.com/filthycritic/recent-shit']
+
+    # reviews pp set by form post. Also takes qs use for now. Mod'd py for changing dropdown at bottom
+    start_urls = ['http://www.millbankusa.com/filthycritic/recent-shit']
+    # start_urls = ['http://www.millbankusa.com/filthycritic/recent-shit?limit=0']
 
     def parse(self, response):
-        pass
+        test = response.css("td a::attr(href)").get()
+        # yield {'res': str(testEl.len())}
+        if test is not None:
+            yield response.follow(test, callback=self.procRev)
+
+    def procRev(self, response):
+        test = response.css("p::text").get()
+        yield {'resFollow': test}
+        # pass
